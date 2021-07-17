73a941515c112a417a7cf0a5b678c3f6555f4eff
kksumm123 <kksumm123@gmail.com>
2021-07-18 00:03:32 +0900

    ray쏴서 바닥인지 체크 , Fall중에만 중력가속도 받도록 , 땅이면 속도 초기화

--- Assets/GravityAcceleration.cs
+++ Assets/GravityAcceleration.cs
@@ -16,19 +16,54 @@ public class GravityAcceleration : MonoBehaviour
     [SerializeField] float gravityVelocity;
     [SerializeField] float s;
 
+    BoxCollider2D boxCol2D;
+    float rayDistance = 0;
+    LayerMask wallLayer;
     void Start()
+    {
+        Init();
+
+        boxCol2D = GetComponent<BoxCollider2D>();
+        rayDistance = boxCol2D.size.y * 0.5f + 0.01f;
+        wallLayer = 1 << LayerMask.NameToLayer("Ground");
+        Debug.Assert(wallLayer != 0, "레이어 지정안됨");
+    }
+
+    void Init()
     {
         gravityAcceleration = 9.81f;
         gravityVelocity = 0;
         s = 0;
     }
 
+    RaycastHit2D ray;
     void Update()
+    {
+        if (IsGround() == false)
+            gravityAccelerationMove();
+        else
+            Init();
+    }
+
+    private bool IsGround()
+    {
+        ray = Physics2D.Raycast(
+                    transform.position, Vector2.down, rayDistance, wallLayer);
+        return ray.transform;
+    }
+    
+    private void gravityAccelerationMove()
     {
         gravityVelocity += gravityAcceleration * Time.deltaTime;
 
         s = gravityVelocity + (0.5f * gravityAcceleration * Mathf.Pow(Time.deltaTime, 2));
 
         transform.Translate(new Vector3(0, -s * Time.deltaTime, 0), Space.World);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, rayDistance * Vector3.down);
     }
 }
