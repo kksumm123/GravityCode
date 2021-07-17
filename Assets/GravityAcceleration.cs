b397807053698f1e7d3adc75a609227bed756f67
kksumm123 <kksumm123@gmail.com>
2021-07-17 23:38:46 +0900

    중력 코드 초안

--- Assets/GravityAcceleration.cs
+++ Assets/GravityAcceleration.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class GravityAcceleration : MonoBehaviour
 {
+    // 속도값 선언하고
+    // 가속도만큼 더해진다
+    // 속도만큼 pos -
+    // t = 시간, g = 중력가속도
+    // v = 속도, s = 이동거리,
+    // 낙하 속도 v1 = v0 + gt
+    // 낙하 시간 s = (v0 * t) + (0.5 * g * t^2)
 
+    [SerializeField] float gravityAcceleration = 9.81f;
+    [SerializeField] float gravityVelocity;
+    [SerializeField] float s;
+    void Start()
+    {
+        gravityAcceleration = 9.81f;
+        gravityVelocity = 0;
+        s = 0;
+    }
+
+    Vector3 pos;
+    void Update()
+    {
+        gravityVelocity += gravityAcceleration * Time.fixedDeltaTime;
+        
+        s = (gravityVelocity * Time.fixedDeltaTime)
+            + (0.5f * gravityAcceleration * Mathf.Pow(Time.fixedDeltaTime, 2));
+
+        transform.Translate(new Vector3(0, -s, 0), Space.World);
+    }
 }
